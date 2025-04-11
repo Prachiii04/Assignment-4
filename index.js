eae7609f1e057c2654a7a48520e6d1c6e1af0a90
Prachiii04 <patelprachi041005@gmail.com>
2025-04-11 13:48:26 -0400

    completed the part of the chapter 2.

---
+++ index.js
@@ -0,0 +1,24 @@
+console.log('hello world')
+
+const testEl = document.getElementById('test-el')
+
+testEl.textContent = 'bye bye'
+
+testEl.addEventListener('click', ()=>{
+  console.log('clicked');
+  testEl.innerHTML ="<b>clicked</b>"
+})
+
+testEl.addEventListener('mouseover', ()=>{
+  console.log('on')
+})
+
+testEl.addEventListener('mouseout', ()=>{
+  console.log('off')
+})
+
+document.addEventListener('scroll',()=>{
+  const positionY = window.scrollY
+  console.log(positionY)
+})
+ 
